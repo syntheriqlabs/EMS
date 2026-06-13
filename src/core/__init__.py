8e018faa7b4421ee43c8c49dd0ca06de1cf278c1
syntheriqlabs <syntheriqlabs@gmail.com>
2026-06-13 10:47:12 -0400

    Create core package with exports

---
+++ src/core/__init__.py
@@ -0,0 +1,4 @@
+from .engine import EMSEngine, EthicalMemory, CoreValuesLayer, ContextualEthicsLayer, RelationalIdentityLayer, ValueTensionAnalyzer
+from .ems_agent import EMSAgent, DialogueManager, EMSResponse
+
+__all__ = ['EMSEngine', 'EthicalMemory', 'CoreValuesLayer', 'ContextualEthicsLayer', 'RelationalIdentityLayer', 'ValueTensionAnalyzer', 'EMSAgent', 'DialogueManager', 'EMSResponse']
